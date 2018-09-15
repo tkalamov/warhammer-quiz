76b62ebaddb5213eb92d5d32b92fcbeab0780fa5
tkalamov <tkalamov@outlook.com>
2018-09-15 08:11:50 +0300

    Add files via upload

---
+++ Quiz app/quotes.js
@@ -0,0 +1,18 @@
+const quotes = [
+    "A good soldier obeys without question. A good officer commands without doubt.",
+    "Blessed is the mind too small for doubt.",
+    "To admit defeat is to blaspheme against the Emperor.",
+    "For those who seek perfection there can be no rest on this side of the grave.",
+    "The difference between heresy and treachery is ignorance.",
+    "Knowledge is power, guard it well.",
+    "An open mind is like a fortress with its gates unbarred and unguarded.",
+    "Innocence proves nothing.",
+    "Success is commemorated; Failure merely remembered.",
+    "Even a man who has nothing can still offer his life.",
+    "Only in death does duty end.",
+    "No man died in His service that died in vain.",
+    "Hope is the first step on the road to disappointment.",
+    "There is no such thing as innocence, only degrees of guilt."
+
+]
+
